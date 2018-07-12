4ab75ae9f018b10e3c5f3a4758bd42daf805e8a1
mrg20 <mariva1995@gmail.com>
2018-07-12 12:52:13 +0200

    added dataframes

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import pandas as pd
 import numpy as np
 
 
-mcdonalds_julio = {'conos': 2234342342, 'mcpollo': 235443453452, 'bigmac': 3453453453454, 'Boston': 23235234}
+mcdonalds_julio = {'conos': 724009138, 'mcpollo': 724009126, 'bigmac': 724008146, 'cbo': 724008200}
+
+julio_completo = {'Codigo': [724009126, 724009018, 724009138], 'Precio': ['4,90€', '3€', '0,50€'], 'Producto': ['Menú McPollo', 'Menú Hamburguesa con Queso o Chicken Burger BBQ', 'Cono']}
 
 
 def simple_series():
     offers = pd.Series(mcdonalds_julio)
     print(offers)
 
+def dataframe():
+    gold_offers = pd.DataFrame(julio_completo)
+    print(gold_offers)
 
 if __name__ == "__main__":
     simple_series()
+    dataframe()
